d71b767b7cbe924bc9543f05adfde2c70d4f5594
Luiz Henrique Gomes <lhgk22235@gmail.com>
2025-05-26 15:24:29 -0400

    Add files via upload

---
+++ testando_definicoes.py
@@ -0,0 +1,51 @@
+
+# compras = ["pão","café","leite"]
+# print(compras)
+
+# #pode ser removido pelo nome ou pelo índice do produt
+# #compras.remove("café")
+# compras.remove(compras[1])
+# print(compras)
+
+# #append acrescenta um item no final da lista (só pode adicionar um por vez).
+# compras.append("açúcar")
+# print(compras)
+
+
+
+# #é preciso criar uma lista nova para receber os elementos ordenados da lista antiga
+# compras_ordenada = sorted(compras)
+# print(compras_ordenada)
+
+# #o nome "item" é o identificador dos itens dentro da lista, esses intens podem receber qualquer tipo de nome
+# for item in compras:
+#     print(f"-{item}")
+
+#TESTE!!!
+
+#lista = []
+#Dicionários = { chave: valor }
+#tuplas = ()
+# conjuntos = set()
+
+#Lista
+
+#teste = [1,2,3,5]
+#print(teste)
+#teste.append(4)
+#print(teste)
+
+#Tupla
+
+# teste = (1,2,3,5)
+# print(teste)
+# teste.append(4)
+# print(teste)
+
+#conjunto
+
+teste = {1,2,3,1,5}
+print(teste)
+# teste.remove(1)
+teste.add(4)
+print(teste)
