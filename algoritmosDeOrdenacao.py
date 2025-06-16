2bf28e8698feddea23aaf63ec5a86bee57040f3a
Luiz Henrique Gomes <lhgk22235@gmail.com>
2025-06-16 15:29:48 -0400

    Add files via upload

---
+++ algoritmosDeOrdenacao.py
@@ -0,0 +1,18 @@
+#lista=[1,3,5,6,7,8,0,2,4,6,8]
+
+# #coloque os elementos em ordem crescente
+
+# # print(sorted(lista))
+
+# #Ordenação sem utilizar a ferramenta "Sorted()"
+
+# for numero in range(len(lista)):
+# #pega os elementos em um distância equivalente ao tamanho da lista
+#     for numero2 in range(len(lista)-1):
+#         if lista[numero2] > lista[numero2 +1]:
+#         #caso o numero no index[0] seja maior que o index[1]
+#             lista[numero2], lista[numero2 +1] = lista[numero2 +1], lista[numero2]
+#             #os elementos trocam de lugar.
+# print(lista)
+
+#ordenação utilizando metodo bubble sort
